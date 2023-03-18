6b694e5d72253e14b27b0cb92788fba6634535eb
thehun <hustyanglin@qq.com>
2023-03-18 23:36:55 +0800

    Initial commit

---
+++ src/test/java/com/thehun/penta/PentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thehun.penta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
